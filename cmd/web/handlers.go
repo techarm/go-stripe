f8f3dfb1dede19eae00d2cda1e9d68af22e3c686
techarm <techarmdev@gmail.com>
2022-04-29 23:09:11 +0900

    stripe初期化処理を追加

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,15 @@ package main
 import "net/http"
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+
+	stringMap := make(map[string]string)
+	stringMap["stripe-key"] = app.config.stripe.key
+
+	var data = &tempalteData{
+		StringMap: stringMap,
+	}
+
+	if err := app.renderTemplate(w, r, "terminal", data); err != nil {
 		app.errorLog.Println(err)
 	}
 }
