7e5679da0fe8f584e989c83354286173ac8893e9
techarm <techarmdev@gmail.com>
2022-05-01 16:54:26 +0900

    backend処理を追加

---
+++ cmd/api/handlers-api.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/techarm/go-stripe/internal/cards"
+)
+
+type stripePayload struct {
+	Currency string `json:"currency"`
+	Amount   string `json:"amount"`
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message,omitempty"`
+	Content string `json:"content,omitempty"`
+	ID      int    `json:"id,omitempty"`
+}
+
+func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
+	var payload stripePayload
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	amount, err := strconv.Atoi(payload.Amount)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: payload.Currency,
+	}
+
+	ok := true
+	pi, msg, err := card.Charge(payload.Currency, amount)
+	if err != nil {
+		app.errorLog.Println(err)
+		ok = false
+	}
+
+	if ok {
+		out, err := json.Marshal(pi)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	} else {
+		j := jsonResponse{
+			OK:      false,
+			Message: msg,
+			Content: "",
+		}
+
+		out, err := json.Marshal(j)
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	}
+}
