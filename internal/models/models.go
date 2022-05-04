fadc1c917464d4046969a8f5c8c72b9879704c14
techarm <techarmdev@gmail.com>
2022-05-04 20:39:45 +0900

    Model定義を追加

--- internal/models/models.go
+++ internal/models/models.go
@@ -26,8 +26,55 @@ type Widget struct {
 	Description    string    `json:"description"`
 	InventoryLevel int       `json:"inventory_level"`
 	Price          int       `json:"price"`
+	Image          string    `json:"image"`
 	CreateAt       time.Time `json:"-"`
 	UpdateAt       time.Time `json:"-"`
+}
+
+type Order struct {
+	ID            int       `json:"id"`
+	WidgetID      int       `json:"widget_id"`
+	TransactionID int       `json:"transaction_id"`
+	StatusID      int       `json:"status_id"`
+	Quantity      int       `json:"quantity"`
+	Amount        int       `json:"amount"`
+	CreateAt      time.Time `json:"-"`
+	UpdateAt      time.Time `json:"-"`
+}
+
+type Status struct {
+	ID       int       `json:"id"`
+	Name     int       `json:"name"`
+	CreateAt time.Time `json:"-"`
+	UpdateAt time.Time `json:"-"`
+}
+
+type TransactionStatus struct {
+	ID       int       `json:"id"`
+	Name     int       `json:"name"`
+	CreateAt time.Time `json:"-"`
+	UpdateAt time.Time `json:"-"`
+}
+
+type Transaction struct {
+	ID                  int       `json:"id"`
+	Amount              int       `json:"amount"`
+	Currency            string    `json:"currency"`
+	LastFour            string    `json:"last_four"`
+	BankReturnCode      string    `json:"bank_return_code"`
+	TransactionStatusID int       `json:"transaction_status_id"`
+	CreateAt            time.Time `json:"-"`
+	UpdateAt            time.Time `json:"-"`
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreateAt  time.Time `json:"-"`
+	UpdateAt  time.Time `json:"-"`
 }
 
 func (m *DBModel) GetWidget(id int) (Widget, error) {
